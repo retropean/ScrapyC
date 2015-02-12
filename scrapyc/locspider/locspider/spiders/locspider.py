12c8631bb900d7acf75ced3b505ba913736d72bf
Eric Randall <eric.randalls@gmail.com>
2015-02-12 10:51:06 -0500

    Locspider fix
    Almost done....?

--- scrapyc/locspider/locspider/spiders/locspider.py
+++ scrapyc/locspider/locspider/spiders/locspider.py
@@ -27,55 +27,36 @@ class LocSpider(Spider):
 		co = 1
 		st = 1
 		ci = 1
+		
 		for site in sites:
 			while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[last()]/li[last()]/a/text()'.format(f=st)).extract()):
 				while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[{f}]/li[last()]/a/text()'.format(f=st)).extract()):
 					item = LocspiderItem()
-					item['city'] = map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract())
-					item['domain'] = map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/@href'.format(j=ci, f=st)).extract())
+					item['city'] = map(unicode.strip, site.xpath('.//div[3]/div/ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract())
+					item['domain'] = map(unicode.strip, site.xpath('.//div[3]/div/ul[{f}]/li[{j}]/a/@href'.format(j=ci, f=st)).extract())
+					item['state'] = map(unicode.strip, site.xpath('.//div[3]/div/h4[{i}]/text()'.format(i = st)).extract())
+					item['continent'] = map(unicode.strip, site.xpath('.//div[3]/div/h1[{i}]/text()'.format(i = co)).extract())
 					ci+=1
 					items.append(item)
 				st+=1
+				ci=1
+			co+=1
+			st=1
 		return items
-				
-
-'''		while map(unicode.strip, site.xpath('.//h1[{i}]/text()'.format(i = co)).extract()) != map(unicode.strip, site.xpath('.//h1[last()]/text()').extract()):
-				while map(unicode.strip, site.xpath('.//h4[{i}]/text()'.format(i = st)).extract()) != map(unicode.strip, site.xpath('.//h4[last()]/text()').extract()):
-					while map(unicode.strip, site.xpath('//ul/li[{j}]/a/text()'.format(j = st)).extract()) != map(unicode.strip, site.xpath('//ul[last()]/li/a/text()').extract()):
-						item = LocspiderItem()	
-						print map(unicode.strip, site.xpath('//li[{j}]/a/text()'.format(j = ci)).extract())
-						item['continent'] = map(unicode.strip, site.xpath('.//h1[{i}]/text()'.format(i = co)).extract())
-						item['state'] = map(unicode.strip, site.xpath('.//h4[{i}]/text()'.format(i = st)).extract())
-						item['city'] = map(unicode.strip, site.xpath('//li[{j}]/a/text()'.format(j = ci)).extract())
-						item['domain'] = map(unicode.strip, site.xpath('.//li[{j}]/a/@href'.format(j = ci)).extract())
-						ci=ci+1
-						items.append(item)
-					st=st+1
-				co+=1
-		return items
-
-#					items.append(item)
-#			print 'hello'
-#			print co
-#			print map(unicode.strip, site.xpath('.//h1[{i}]/text()'.format(i = co)).extract())
-#			print map(unicode.strip, site.xpath('.//h1[last()]/text()').extract())
-#			print site.xpath('.//h1[last()]/text()').count()
-			
-		#return items
-			
+'''				
 		for site in sites:
-			item = LocspiderItem()
-			item['continent'] = map(unicode.strip, site.xpath('../../../../h1[last()]/text()').extract())
-#			item['state'] = map(unicode.strip, site.xpath('../../h4[1]/text()').extract())
-			item['state'] = map(unicode.strip, site.xpath('h4/text()[preceding-sibling::../ul]').extract())
-			item['city'] = map(unicode.strip, site.xpath('.//text()').extract())
-			item['domain'] = map(unicode.strip, site.xpath('.//@href').extract())
-			items.append(item)
+			while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[last()]/li[last()]/a/text()'.format(f=st)).extract()):
+				while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[{f}]/li[last()]/a/text()'.format(f=st)).extract()):
+					item = LocspiderItem()
+					item['city'] = map(unicode.strip, site.xpath('.//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract())
+					item['domain'] = map(unicode.strip, site.xpath('.//ul[{f}]/li[{j}]/a/@href'.format(j=ci, f=st)).extract())
+					item['state'] = map(unicode.strip, site.xpath('.//h4[{i}]/text()'.format(i = st)).extract())
+					item['continent'] = map(unicode.strip, site.xpath('.//h1[{i}]/text()'.format(i = co)).extract())
+					ci+=1
+					items.append(item)
+				st+=1
+				ci=1
+			co+=1
+			st=1
 		return items
-			for con in sel.xpath('.//h1/h4/li'):
-				item['continent'] = map(unicode.strip, site.xpath('.//h1/text()').extract())
-				for s in sel.xpath('.//h4/text()'):
-					item['state'] = map(unicode.strip, site.xpath('.//h4/text()').extract())
-#					for cit in site.xpath('.//li/a/@href'):
-#						item['city'] = map(unicode.strip, site.xpath('.//li/a/text()').extract())
-#						item['domain'] = map(unicode.strip, site.xpath('.//li/a/@href').extract())'''
+'''
