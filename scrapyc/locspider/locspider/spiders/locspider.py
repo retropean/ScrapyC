d4f0cd662305633230130efb382bc3e1c95abb18
Eric Randall <eric.randalls@gmail.com>
2015-02-12 16:36:07 -0500

    locspider 2
    manditory description, almost ready for merge

--- scrapyc/locspider/locspider/spiders/locspider.py
+++ scrapyc/locspider/locspider/spiders/locspider.py
@@ -20,14 +20,34 @@ class LocSpider(Spider):
 
 	def parse(self, response):
 		sel = Selector(response)
-#		sites = sel.xpath('//div/ul/li')
-#		sites = sel.xpath('//section[@class="body"]/h1')
 		sites = sel.xpath('//section[@class="body"]')
 		items = []
 		co = 1
 		st = 1
 		ci = 1
-		
+		divacounter = 1
+		divbcounter = 1
+
+		for site in sites:
+			while map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[last()+1]/ul[last()+1]/li[last()+1]/a/text()'.format(j=ci, e=divacounter, k=divbcounter)).extract()) != map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[{k}]/ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st, e=divacounter, k=divbcounter)).extract()):	
+				#get all objects within one div tag (first row of states)
+				while map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[{k}]/ul[last()+1]/li[last()+1]/a/text()'.format(j=ci, e=divacounter, k=divbcounter)).extract()) != map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[{k}]/ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st, e=divacounter, k=divbcounter)).extract()):
+					#get all objects within one ul tag
+					while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[{f}]/li[last()+1]/a/text()'.format(f=st)).extract()):
+						item = LocspiderItem()
+						item['city'] = map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[{k}]/ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st, e=divacounter, k=divbcounter)).extract())
+						item['domain'] = map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[{k}]/ul[{f}]/li[{j}]/a/@href'.format(j=ci, f=st, e=divacounter, k=divbcounter)).extract())
+						item['state'] = map(unicode.strip, site.xpath('.//div[@class="colmask"][{e}]/div[{k}]/h4[{i}]/text()'.format(i = st, e=divacounter, k=divbcounter)).extract())
+						item['continent'] = map(unicode.strip, site.xpath('.//h1[{i}]/text()'.format(i = co)).extract())
+						ci+=1
+						items.append(item)
+					st+=1
+					ci=1
+				divbcounter+=1
+				ci=1
+				st=1
+		return items
+'''	
 		for site in sites:
 			while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[last()]/li[last()]/a/text()'.format(f=st)).extract()):
 				while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[{f}]/li[last()]/a/text()'.format(f=st)).extract()):
@@ -44,19 +64,3 @@ class LocSpider(Spider):
 			st=1
 		return items
 '''				
-		for site in sites:
-			while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[last()]/li[last()]/a/text()'.format(f=st)).extract()):
-				while map(unicode.strip, site.xpath('//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract()) != map(unicode.strip, site.xpath('//ul[{f}]/li[last()]/a/text()'.format(f=st)).extract()):
-					item = LocspiderItem()
-					item['city'] = map(unicode.strip, site.xpath('.//ul[{f}]/li[{j}]/a/text()'.format(j=ci, f=st)).extract())
-					item['domain'] = map(unicode.strip, site.xpath('.//ul[{f}]/li[{j}]/a/@href'.format(j=ci, f=st)).extract())
-					item['state'] = map(unicode.strip, site.xpath('.//h4[{i}]/text()'.format(i = st)).extract())
-					item['continent'] = map(unicode.strip, site.xpath('.//h1[{i}]/text()'.format(i = co)).extract())
-					ci+=1
-					items.append(item)
-				st+=1
-				ci=1
-			co+=1
-			st=1
-		return items
-'''
